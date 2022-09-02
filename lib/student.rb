33cef8124c28ccd6e0aa0cb9082357c8982394f9
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-09-02 11:10:14 +0000

    Modified user.rb && student.rb && teacher.rb

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize(knowledge=[])
+        @knowledge = knowledge
+    end
+
+    def learn(item)
+        @knowledge << item
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
